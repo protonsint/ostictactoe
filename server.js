a99f194ab2448a15d25b8c5693358c0f42b22a7a
protonsint <protonsint@gmail.com>
2016-06-26 03:25:59 +0530

    updated

--- server.js
+++ server.js
@@ -1,33 +1,25 @@
+// Check the configuration file for more details
+var config = require('./config');
+
 var express = require('express');
 var app = express()
 app.use('/tictactoe', express.static(__dirname + '/main'));
 socket = require('socket.io');
-var config = require('./config');
 
 
 
-var port = config.serverport;
-/* app.listen(port, function() {
+
+/*var port = process.env.PORT || 8080;
+ app.listen(port, function() {
   //console.log("Express server listening on port %d", app.address().port); 
 }); */
-/* 
-var listener = app.listen( port, function(){
+
+var listener = app.listen( config.serverport, function(){
     console.log('Listening on port ' + listener.address().port); //Listening on port 8888
     console.log('/tictactoe'); //Handler
+	
+	
 });
-
- */
-
-
-
-/* console.log("Trying to start server with config:", config.serverip + ":" + config.serverport);
- */
-// Both port and ip are needed for the OpenShift, otherwise it tries 
-// to bind server on IP 0.0.0.0 (or something) and fails
-/* server.listen(config.serverport, config.serverip, function() {
-  console.log("Server running @ http://" + config.serverip + ":" + config.serverport);
-});
- */
 
 
 
@@ -160,6 +152,6 @@ var leaveTable = function (socket) {
     }
 };
 
-http.listen(port, function () {
-    console.log('listening on *:'+port);
+http.listen(3000, function () {
+    console.log('listening on *:3000');
 });
